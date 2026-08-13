466acbea3386a3dbce405a9e7b3dccdeaf214def
Noor Fatima <noor2239064@gmail.com>
2026-08-13 23:55:04 +0500

    made the sidebar responsive

--- src/HOST COMPONENETS/HostLanding.jsx
+++ src/HOST COMPONENETS/HostLanding.jsx
@@ -6,20 +6,74 @@ import ApprovalPending from "./ApprovalPending";
 import { useState } from "react";
 
 function HostLanding() {
-  const [bar, setBar] = useState();
+  const [bar, setBar] = useState(false);
   return (
     <>
-      <div className="top-bar w-full bg-gray-100 flex items-center">
+      {/* BAR */}
+      <div className="top-bar w-full bg-gray-100 flex items-center md:hidden shadow-md">
         <div className=" wrapper flex items-center justify-between">
           <div className="flex flex-col">
             <h1 className="text-[18px] font-bold">EVENTS</h1>
             <p className="text-[13px] italic">Organizer's Dashboard</p>
           </div>
-          <i className="fa-solid fa-bars text-[18px]"></i>
+          <i
+            onClick={() => setBar(!bar)}
+            className="fa-solid fa-bars text-[18px] cursor-pointer"
+          ></i>
         </div>
       </div>
+      {/* MOBILE */}
+      <div className="md:hidden flex">
+        <main>
+          <Outlet />
+        </main>
 
-      <div className="sidebar-container h-dvh flex text-[#2e2e2e] md:block hidden">
+        {bar && (
+          <aside className="h-dvh bg-gray-100 absolute right-0 w-[180px]">
+            <div className="aside-content wrapper h-[85%]">
+              <div className="aside-items wrapper flex flex-col h-[85%] justify-between">
+                <div className="aside-pages flex flex-col gap-5">
+                  <Link
+                    to="."
+                    className="flex justify-between items-center cursor-pointer"
+                  >
+                    <p className="text-[13px]">Dashboard</p>
+                    <i className="fa-solid fa-caret-right text-[#b8b8b8] text-[10px]"></i>
+                  </Link>
+                  <Link
+                    to={"post-event"}
+                    className="flex justify-between items-center cursor-pointer"
+                  >
+                    <p className="text-[13px]">Post an event</p>
+                    <i className="fa-solid fa-caret-right text-[#b8b8b8] text-[10px]"></i>
+                  </Link>
+                  <Link
+                    to={"events-live"}
+                    className="flex justify-between items-center cursor-pointer"
+                  >
+                    <p className="text-[13px]">Events Live</p>
+                    <i className="fa-solid fa-caret-right text-[#b8b8b8] text-[10px]"></i>
+                  </Link>
+                  <Link
+                    to={"pending-approvals"}
+                    className="flex justify-between items-center cursor-pointer"
+                  >
+                    <p className="text-[13px]">Pending Approvals</p>
+                    <i className="fa-solid fa-caret-right text-[#b8b8b8] text-[10px]"></i>
+                  </Link>
+                </div>
+
+                <button className="bg-purple-900 hover:bg-purple-800 transition-all duration-300 cursor-pointer text-white font-bold py-1 rounded-2xl">
+                  Log Out
+                </button>
+              </div>
+            </div>
+          </aside>
+        )}
+      </div>
+
+      {/* DESKTOP  */}
+      <div className="sidebar-container h-dvh md:flex hidden text-[#2e2e2e]">
         <aside className="bg-gray-100 lg:w-[250px] md:w-[210px] flex flex-col border-r-2 border-gray-200 h-dvh">
           <div className="aside-items wrapper flex flex-col h-full justify-between">
             <div>
